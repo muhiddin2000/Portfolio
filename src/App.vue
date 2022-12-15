8e6f55d040ac641db920c9bbfc75a0bd97f31392
unknown <muxiddinmalikov7@gmail.com>
2022-12-14 21:04:19 -0800

    frontend

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+
+<div >
+    <Main />
+    <router-view :key="$route.fullPath" />
+</div>
+
+
+</template>
+
+<style lang="scss">
+</style>
+
+<script>
+
+import Main from '@/views/Main.vue'
+
+export default {
+    name: "App",
+    components: {
+        Main,
+       
+
+    }
+}
+</script>
